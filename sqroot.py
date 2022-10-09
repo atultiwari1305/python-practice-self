7c85080cd231d6c83106462ffd48ac7db0671987
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2022-10-09 12:12:29 +0530

    modified

--- sqroot.py
+++ sqroot.py
@@ -1,5 +1,5 @@
 #find the square root of any number
 
-x=input("Enter your number: ")
-a = int(x) ** 0.5
+x=float(input("Enter your number: "))
+a = x ** 0.5
 print("The square root of the number is: ", a)
