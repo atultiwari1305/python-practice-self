39051cf123fbfbf44d85dfff5247108dabd5d1a5
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2022-10-16 18:45:14 +0530

    completed

---
+++ lastdigit.py
@@ -0,0 +1,7 @@
+a=str(input("Enter your number: "))
+x=a[-1]
+print(x)
+if int(x)%3==0:
+    print("yes")
+else:
+    print("no")
---
+++ prime_no.py
@@ -0,0 +1,4 @@
+print("HELLO USER")
+x=float(input("Enter your Number: "))
+
+if (X%1)
