974787fe43ccf891c0c4dbf1724b0c83b97e38a8
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2023-02-08 11:31:29 +0530

    per

--- percentage.py
+++ percentage.py
@@ -1,3 +1,3 @@
-num1=int(input("enter your number: "))
-num2=int(input("enter your max no.: "))
+num1=int(input("Enter your number: "))
+num2=int(input("Enter your max no.: "))
 print((num1/num2)*100)
